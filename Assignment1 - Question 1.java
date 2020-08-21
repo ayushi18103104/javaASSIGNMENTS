74565a89341fae3eafc2e7ce00221ab251cea41b
ayushi18103104 <69955558+ayushi18103104@users.noreply.github.com>
2020-08-21 23:31:57 +0530

    Update Assignment1 - Question 1.java

--- Assignment1 - Question 1.java
+++ Assignment1 - Question 1.java
@@ -1,29 +1,86 @@
 import java.util.Scanner;
-class solution { 
-      
-    static int count(String pattern, String text) {         
-        int psize = pattern.length();         
-        int tsize = text.length();         
-        int res = 0; 
 
-        for (int i = 0; i <= tsize - psize; i++) { 
-            int j;             
-            for (j = 0; j < psize; j++) { 
-                if (text.charAt(i + j) != pattern.charAt(j)) { 
-                    break; 
-                } 
-            } 
-            if (j == psize) {                 
-                res++;                 
-                j = 0;                 
-            }             
-        }         
-        return res;         
-    } 
-    static public void main(String[] args) { 
-        Scanner sc= new Scanner(System.in);
-        String txt = sc.nextLine();         
-        String pat = sc.nextLine();         
-        System.out.println(count(pat, txt));         
-    } 
-} 
+class solution
+{ 
+	void lpsarr(String pattern, int m, int lps[]) 
+	{ 
+		int l = 0, i=1; 
+		lps[0] = 0;
+
+		while (i < m) 
+		{ 
+			if (pattern.charAt(i) == pattern.charAt(l)) 
+			{ 
+				l++; 
+				lps[i] = ln; 
+				i++; 
+			} 
+			else 
+			{ 
+				if (l != 0) 
+				{ 
+					l = lps[l-1]; 
+				} 
+				else
+				{ 
+					lps[i] = l; 
+					i++; 
+				} 
+			} 
+		}
+	}
+
+	int search(String text, String pattern) 
+	{ 
+		int m = pattern.length(); 
+		int n = text.length(); 
+
+		int lps[] = new int[m]; 
+		int j = 0; 
+
+		lpsarray(pattern,m,lps); 
+
+		int i = 0, count = 0, next_i = 0; 
+		
+		while (i < n) 
+		{ 
+			if (pattern.charAt(j) == text.charAt(i)) 
+			{ 
+				j++; 
+				i++; 
+			} 
+			if (j == m) 
+			{ 
+				j = lps[j-1]; 
+				count++; 
+
+				if (lps[j]!=0) 
+					i = ++next_i; 
+				j = 0; 
+			} 
+
+			else if (i < n && pattern.charAt(j) != text.charAt(i)) 
+			{ 
+				if (j != 0) 
+					j = lps[j-1]; 
+				else
+					i = i+1; 
+			} 
+		} 
+		return count; 
+	}
+
+	public static void main(String args[]) 
+	{ 
+		Scanner obj = new Scanner(System.in);
+		
+		System.out.println("Enter text ");
+		String str1 = obj.nextLine();
+		
+		System.out.println("Enter the pattern to be searched for ");
+		String str2 = obj.nextLine();
+		
+		int count = new Q1().search(str1,str2); 
+		System.out.println(count); 
+	}
+}
