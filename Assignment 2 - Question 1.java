0e8b7de974fc4dcaac284eb914301173eb60a907
ayushi18103104 <69955558+ayushi18103104@users.noreply.github.com>
2020-08-31 16:12:23 +0530

    Add files via upload

---
+++ Assignment 2 - Question 1.java
@@ -0,0 +1,44 @@
+import java.io.*; 
+import java.lang.*; 
+import java.util.*;
+public class mySolution
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the first string");
+		String inp1 =sc.nextLine();
+		System.out.println("Enter the second string");
+		String inp2 = sc.nextLine();
+		
+		char str1[] = inp1.toCharArray();
+		char str2[] = inp2.toCharArray();
+		
+		
+		int flag = 1;
+		
+		if(inp1.length() != inp2.length())
+		{
+			System.out.println("Not Equal");
+		}
+		else
+		{
+			for(int i=0;i<inp1.length();i++)
+			{
+				if(str1[i] != str2[i]) 
+					flag = 0;
+			}
+		}
+		
+		if(flag == 1)
+		{
+			System.out.println("Equal Strings");
+		}
+		else
+		{
+			System.out.println("Not Equal");
+		}
+		
+		
+	}
+}
